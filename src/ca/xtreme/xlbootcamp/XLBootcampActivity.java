190cac7f39b1fca10e5897d2cdbeecbcc081ace3
Grace Li <grace.li@utoronto.ca>
2012-04-29 13:46:34 -0400

    Refactored and cleaned the code a little

--- src/ca/xtreme/xlbootcamp/XLBootcampActivity.java
+++ src/ca/xtreme/xlbootcamp/XLBootcampActivity.java
@@ -29,87 +29,63 @@ public class XLBootcampActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.main);
-
-        // grab tweets from twitter
         
-        //what is entity?
-        //I think this is passing in parameters to the base URI
-//        final HttpEntity entity;
-//        try {
-//        	entity = new UrlEncodedFormEntity(params);
-//        } catch (final UnsupportedEncodingException e) {
-//        	throw new IllegalStateException(e);
-//        }
-        final HttpResponse response;
-        final HttpGet get = new HttpGet(URI);
-        ArrayList<String> tweetMessageList = new ArrayList<String>();
+        //what is HTTPEntity?
+        ArrayList<String> tweets = getTweets();
         
-        try {
-        	Log.d(TAG, "Http get from Twitter search api");
-        	response = getHttpClient().execute(get);
-        	
-        	//parse data returned via the response object
-        	Log.d(TAG, "Parse data from the response");
-        	BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-        	StringBuilder builder = new StringBuilder();
-        	
-        	for (String line = null; (line = reader.readLine()) != null;) {
-        	    builder.append(line).append("\n");
-        	}
-        	
-        	JSONTokener tokener = new JSONTokener(builder.toString());
-        	
-        	//create a JSONObject so that I get get values using keys
-        	try {
-				JSONObject jsonObj = new JSONObject(tokener);
-				//create an array of Strings of just tweet messages
-				JSONArray results = jsonObj.getJSONArray("results");
-				Log.d(TAG, "json results from twitter:" + results.toString());
-//				for each result
-//					create a JSONObject representing the tweet
-//					get only the message (text field in the json response)
-//					append it to a list of Java strings
-				for(int i=0; i<results.length(); i++) {
-					JSONObject aTweet = results.getJSONObject(i);
-					String message = aTweet.getString("text");
-					tweetMessageList.add(message);
-				}
-			} catch (JSONException e1) {
-				// TODO Auto-generated catch block
-				Log.e(TAG, "JSONException problem creating JSONObject from tokener:", e1);
-			}
-        	
-//        	try {
-//				JSONArray finalResult = new JSONArray(tokener);
-//				
-//				//parse into a array of strings
-//				
-//				//code to test whether the get call was made properly
-////				String jsonString = finalResult.toString();
-////				Log.d(TAG, "pulled json data");
-////				Log.d(TAG, jsonString);
-//			} catch (JSONException e) {
-//				Log.e(TAG, "JSONException", e);
-//			}
-        } catch (final IOException e) {
-        	Log.e(TAG, "IOException", e);
-        	Log.d(TAG, "IOException: Message: " + e.getMessage());
-        } finally {
-        	Log.v(TAG, "Complete");
-        }
-        
-//        String[] countries = getResources().getStringArray(R.array.countries_array);
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, tweetMessageList));
+        //populate a list view with tweets
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, tweets));
     }
     
-    // creates and returns an http client object to call twitter search
-    public static HttpClient getHttpClient() {
+    // Gets and parses the latest tweets from Twitter search API
+    private ArrayList<String> getTweets() {
+       ArrayList<String> tweetMessageList = new ArrayList<String>();
+       final HttpResponse response;
+       final HttpGet get = new HttpGet(URI);
+       
+       try {
+    	   Log.d(TAG, "Http get from Twitter search api");
+    	   response = getHttpClient().execute(get);
+
+    	   Log.d(TAG, "Parse data from the response");
+    	   //read and serial JSON data into a String
+    	   BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+    	   StringBuilder builder = new StringBuilder();
+    	   for (String line = null; (line = reader.readLine()) != null;) {
+    		   builder.append(line).append("\n");
+    	   }
+
+    	   //parse and extract the data in JSON
+    	   JSONTokener tokener = new JSONTokener(builder.toString());
+    	   //create a JSONObject so that I can get values corresponding to keys
+    	   try {
+    		   JSONObject jsonObj = new JSONObject(tokener);
+    		   
+    		   //create an array of Strings of just tweet messages
+    		   JSONArray results = jsonObj.getJSONArray("results");
+    		   
+    		   //Log.d(TAG, "json results from twitter:" + results.toString());
+
+    		   for(int i=0; i<results.length(); i++) {
+    			   JSONObject aTweet = results.getJSONObject(i);
+    			   String message = aTweet.getString("text");
+    			   tweetMessageList.add(message);
+    		   }
+    	   } catch (JSONException e) {
+    		   Log.e(TAG, "JSONException problem creating JSONObject from tokener:", e);
+    	   }
+       } catch (final IOException e) {
+    	   Log.e(TAG, "IOException", e);
+    	   Log.d(TAG, "IOException: Message: " + e.getMessage());
+       } finally {
+    	   Log.v(TAG, "Complete");
+       }
+       
+       return tweetMessageList;
+	}
+
+    private HttpClient getHttpClient() {
     	HttpClient httpClient = new DefaultHttpClient();
-//    	final HttpParams params = httpClient.getParams();
-//        HttpConnectionParams.setConnectionTimeout(params, HTTP_REQUEST_TIMEOUT_MS);
-//        HttpConnectionParams.setSoTimeout(params, HTTP_REQUEST_TIMEOUT_MS);
-//        ConnManagerParams.setTimeout(params, HTTP_REQUEST_TIMEOUT_MS);
     	return httpClient;
     }
     

a93d62e87ff7cce6d3132a8348a1ba5b4211565c
Grace Li <grace.li@utoronto.ca>
2012-04-29 18:27:18 -0400

    Implemented feed refresh

--- gen/ca/xtreme/xlbootcamp/R.java
+++ gen/ca/xtreme/xlbootcamp/R.java
@@ -16,6 +16,9 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int button=0x7f060000;
+    }
     public static final class layout {
         public static final int list_item=0x7f030000;
         public static final int main=0x7f030001;
@@ -23,5 +26,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
+        public static final int nothing_here_to_see_=0x7f040003;
+        public static final int refresh=0x7f040002;
     }
 }
--- src/ca/xtreme/xlbootcamp/XLBootcampActivity.java
+++ src/ca/xtreme/xlbootcamp/XLBootcampActivity.java
@@ -18,77 +18,91 @@ import android.app.ListActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class XLBootcampActivity extends ListActivity {
+public class XLBootcampActivity extends ListActivity implements OnClickListener {
     /** Called when the activity is first created. */
 	
 	public static final String URI = "http://search.twitter.com/search.json?q=%23bieber";
 	public static final int HTTP_REQUEST_TIMEOUT_MS = 30 * 1000;
 	private static final String TAG = "XLBootcamp";
-
+//	private twitterTask = new GetFromTwitterTask();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        //find button and attach listener to it
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        
+        //populate with feeds when app is launched
         new GetFromTwitterTask().execute(URI);
     }
     
     // Gets and parses the latest tweets given a uri Twitter search API
     private ArrayList<String> getTweets(String uri) {
-       ArrayList<String> tweetMessageList = new ArrayList<String>();
-       final HttpResponse response;
-       final HttpGet get = new HttpGet(uri);
-       
-       try {
-    	   Log.d(TAG, "Http get from Twitter search api");
-    	   response = getHttpClient().execute(get);
+    	ArrayList<String> tweetMessageList = new ArrayList<String>();
+    	final HttpResponse response;
+    	final HttpGet get = new HttpGet(uri);
 
-    	   Log.d(TAG, "Parse data from the response");
-    	
-    	   //read and serial JSON data into a String
-    	   BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-    	   StringBuilder builder = new StringBuilder();
-    	   
-    	   for (String line = null; (line = reader.readLine()) != null;) {
-    		   builder.append(line).append("\n");
-    	   }
+    	try {
+    		Log.d(TAG, "Http get from Twitter search api");
+    		response = getHttpClient().execute(get);
 
-    	   //parse and extract the data in JSON
-    	   JSONTokener tokener = new JSONTokener(builder.toString());
-    	   
-    	   //create a JSONObject so that I can get values corresponding to keys
-    	   try {
-    		   JSONObject jsonObj = new JSONObject(tokener);
-    		   
-    		   //create an array of Strings of just tweet messages
-    		   JSONArray results = jsonObj.getJSONArray("results");
-    		   
-    		   //Log.d(TAG, "json results from twitter:" + results.toString());
+    		Log.d(TAG, "Parse data from the response");
 
-    		   for(int i=0; i<results.length(); i++) {
-    			   JSONObject aTweet = results.getJSONObject(i);
-    			   String message = aTweet.getString("text");
-    			   tweetMessageList.add(message);
-    		   }
-    	   } catch (JSONException e) {
-    		   Log.e(TAG, "JSONException problem creating JSONObject from tokener:", e);
-    	   }
-       } catch (final IOException e) {
-    	   Log.e(TAG, "IOException", e);
-    	   Log.d(TAG, "IOException: Message: " + e.getMessage());
-       } finally {
-    	   Log.v(TAG, "Complete");
-       }
-       
-       return tweetMessageList;
-	}
+    		//read and serial JSON data into a String
+    		BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+    		StringBuilder builder = new StringBuilder();
+
+    		for (String line = null; (line = reader.readLine()) != null;) {
+    			builder.append(line).append("\n");
+    		}
+    		
+    		Log.d(TAG, builder.toString());
+    		
+    		//parse and extract the data in JSON
+    		JSONTokener tokener = new JSONTokener(builder.toString());
+
+    		//create a JSONObject so that I can get values corresponding to keys
+    		try {
+    			JSONObject jsonObj = new JSONObject(tokener);
+
+    			//create an array of Strings of just tweet messages
+    			JSONArray results = jsonObj.getJSONArray("results");
+
+    			//Log.d(TAG, "json results from twitter:" + results.toString());
+
+    			for(int i=0; i<results.length(); i++) {
+    				JSONObject aTweet = results.getJSONObject(i);
+    				String message = aTweet.getString("text");
+    				tweetMessageList.add(message);
+    			}
+    		} catch (JSONException e) {
+    			Log.e(TAG, "JSONException problem creating JSONObject from tokener:", e);
+    		}
+    	} catch (final IOException e) {
+    		Log.e(TAG, "IOException", e);
+    		Log.d(TAG, "IOException: Message: " + e.getMessage());
+    	} finally {
+    		Log.v(TAG, "Complete");
+    	}
+
+    	return tweetMessageList;
+    }
 
     private HttpClient getHttpClient() {
     	// todo learn about HttpEntity and related classes 
     	HttpClient httpClient = new DefaultHttpClient();
     	return httpClient;
     }
-    
+
     // todo learn about java generics and how to parameterize asynctask properly
     private class GetFromTwitterTask extends AsyncTask<String, Integer, ArrayList<String>> {
     	@Override
@@ -96,14 +110,21 @@ public class XLBootcampActivity extends ListActivity {
     		//pull tweets from twitter
     		return getTweets(uri[0]);
     	}
-    	
+
     	@Override
     	protected void onPostExecute(ArrayList<String> result) {
     		//populate the list view container with tweets
     		//todo learn why I can get the context this in this way.
-    		setListAdapter(new ArrayAdapter<String>(XLBootcampActivity.this, R.layout.list_item, result));
+    		ListView list = (ListView) findViewById(android.R.id.list);
+    		list.setAdapter(new ArrayAdapter<String>(XLBootcampActivity.this, R.layout.list_item, result));
     	}
     }
+
+	public void onClick(View v) {
+		// refresh feed when the button is clicked
+		Log.d(TAG, "Refreshed feed");
+		new GetFromTwitterTask().execute(URI);
+	}
 }
 
 
